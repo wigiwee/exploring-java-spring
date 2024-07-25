49ec0a8d73fa82d8b4859fe20a32bfdb9ca50dcd
kaustubhKolhe99 <kaustubhkolhe822@outlook.com>
2024-07-25 20:52:05 +0530

    jpa: custom query in JpaRepository

--- spring-boot/jpa/src/main/java/com/jpa/jpa/entities/Category.java
+++ spring-boot/jpa/src/main/java/com/jpa/jpa/entities/Category.java
@@ -2,7 +2,9 @@ package com.jpa.jpa.entities;
 
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -34,7 +36,11 @@ public class Category {
     // private Product product;
 
     //multiple products belongs to single category
-    @OneToMany(mappedBy = "category")   //won't create a extra table 
+    @OneToMany(
+        mappedBy = "category",      //won't create a extra table  
+        cascade = CascadeType.ALL,  // when a category is deleted the products will be deleted
+        fetch = FetchType.EAGER     //when parent will load child will also load,   // in case of LAZY the child will load when it is fetched and not when the parent is loaded
+    )
     private List<Product> products;
 
 
--- spring-boot/jpa/src/main/java/com/jpa/jpa/repositories/ProductRepository.java
+++ spring-boot/jpa/src/main/java/com/jpa/jpa/repositories/ProductRepository.java
@@ -1,11 +1,48 @@
 package com.jpa.jpa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.jpa.jpa.entities.Category;
 import com.jpa.jpa.entities.Product;
+import java.util.List;
+
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    //naming convention of predefined methods of JpaRepository
+
+    //predefined methods
+    List<Product> findByPrice(int price);
     
+    List<Product> findByCategory(Category category);
+
+    //no need to write the method 
+    List<Product> findByTitle(String title);
+
+    boolean existsByTitle(String title);    //return true if product with title exists
+
+    //return the count 
+    int countByPrice(int price);
+    
+    List<Product> findByLive(boolean live);
+
+    // ----------------------------------------------------------
+    //custom method using native query or JPQL/HQL
+
+    @Query("select p from Product p ")
+    List<Product> getMyCustomQueryProduct();
+
+    //native query
+    @Query(nativeQuery = true, value = "select * from jpa_products")
+    List<Product> getMyCustomNativeQueryProduct();
+
+    //with parameters
+
+    @Query("select p from Product p Where p.title =: title and p.price =: price")
+    List<Product> getProductByTitle(@Param("title") String title, 
+        @Param("price") double price);
+
 }
