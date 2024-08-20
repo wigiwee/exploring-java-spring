e51f037f1c5717d610155501d397c6b36017aa95
kaustubhKolhe99 <kaustubhkolhe822@outlook.com>
2024-08-21 00:15:08 +0530

    created new spring-boot project

---
+++ spring-boot/security/jwt-secutiry/src/main/java/com/secutiry/jwt_secutiry/JwtSecutiryApplication.java
@@ -0,0 +1,13 @@
+package com.secutiry.jwt_secutiry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecutiryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSecutiryApplication.class, args);
+	}
+
+}
---
+++ spring-boot/security/jwt-secutiry/src/test/java/com/secutiry/jwt_secutiry/JwtSecutiryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secutiry.jwt_secutiry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSecutiryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
